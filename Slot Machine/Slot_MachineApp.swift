242581e4f3cfbf01c5678bb21b5cfc64d3df5e15
Alex Vasyliev <olorin2007@gmail.com>
2023-05-20 15:29:16 +0200

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Oleksii Vasyliev on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
